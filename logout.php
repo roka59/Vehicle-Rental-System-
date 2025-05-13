71c3492e601482ee6a4700d425acfb50ec15bb9d
Samir Gharti Magar <sghartim0@gmail.com>
2025-05-13 19:11:27 +1000

    emtying
    empty for resending

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-session_start();
-$_SESSION['success'] = "You have been logged out successfully.";
-header("Location: login.php");
-exit();
