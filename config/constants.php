fb13737ec1536af124f8455f06ec91891e187b0a
Aashish Roka <aashish.roka64@gmail.com>
2025-05-13 23:00:29 +1000

    added constants file

---
+++ config/constants.php
@@ -0,0 +1,5 @@
+<?php
+// Adjust according to your folder structure
+define("BASE_URL", "/vehicle-rental-system"); // for use in URLs
+define("BASE_PATH", $_SERVER['DOCUMENT_ROOT'] . "/vehicle-rental-system"); // for includes
+?>
